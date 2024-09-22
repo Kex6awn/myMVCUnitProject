a3973b21d2f191d2fcb458017d8e0fefbb6a62a9
francis.ferdinard <francis.ferdinard@PROGRAM1>
2024-09-22 17:12:54 -0500

    Values Getter and setters

--- myMVCUnitProject/Models/calModels.cs
+++ myMVCUnitProject/Models/calModels.cs
@@ -1,6 +1,16 @@
-﻿namespace myMVCUnitProject.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace myMVCUnitProject.Models
 {
     public class calModels
     {
+        [Required]
+        public int FirstValue { get; set; }
+
+        [Required]
+        public int SecondValue { get; set; }
+
+        public int? FinalValue { get; set; }
+
     }
 }
