33f34a0d4e34f14dc52dc54193b47c616b72a1b2
francis.ferdinard <francis.ferdinard@PROGRAM1>
2024-09-22 17:16:19 -0500

    GET AND POST

--- myMVCUnitProject/Controllers/HomeController.cs
+++ myMVCUnitProject/Controllers/HomeController.cs
@@ -1,12 +1,16 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using myMVCUnitProject.Models;
 
 namespace myMVCUnitProject.Controllers
 {
     public class HomeController : Controller
     {
+        // HTTP GET AND POST ACTION VERBS
+
+        [HttpGet]
         public IActionResult Index()
         {
-            return View();
+            return View(new calModels());
         }
     }
 }
