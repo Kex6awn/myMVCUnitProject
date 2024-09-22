0e227a6a67a8199f95722fd63fa588994f326ca0
francis.ferdinard <francis.ferdinard@PROGRAM1>
2024-09-22 17:28:21 -0500

    calculating final fvalue

--- myMVCUnitProject/Controllers/HomeController.cs
+++ myMVCUnitProject/Controllers/HomeController.cs
@@ -12,5 +12,33 @@ namespace myMVCUnitProject.Controllers
         {
             return View(new calModels());
         }
+
+        [HttpPost]
+        public IActionResult Index(calModels model)
+        {
+            if (ModelState.IsValid)
+            {
+                switch (model)
+                {
+                    case "Add":
+                        model.FinalValue = model.FirstValue + model.SecondValue;
+                        break;
+                    case "Subtract":
+                        model.FinalValue = model.FirstValue - model.SecondValue;
+                        break;
+                    case "Multiply":
+                        model.FinalValue = model.FirstValue * model.SecondValue;
+                        break;
+                    case "Divide":
+                        model.FinalValue = model.SecondValue != 0 ? model.FirstValue / model.SecondValue : (int?)null;
+                        break;
+                    default:
+                        ModelState.AddModelError("", "Invalid operation selected.");
+                        break;
+                }
+            }
+
+            return View(model);
+        }
     }
 }
